0acc3ac57d02290d9eb19cf0efcb0640d5afda8d
Lauren Leatherby <lauren.leatherby@ft.com>
2017-07-12 23:54:40 -0400

    another text update

--- config/article.js
+++ config/article.js
@@ -34,8 +34,7 @@ export default () => ({ // eslint-disable-line
     // url: 'https://image.webservices.ft.com/v1/images/raw/http%3A%2F%2Fcom.ft.imagepublish.prod.s3.amazonaws.com%2Fc4bf0be4-7c15-11e4-a7b8-00144feabdc0?source=ig&fit=scale-down&width=700',
   },
 
-  // Byline can by a plain string, markdown, or array of authors
-  // if array of authors, url is optional
+
   byline: [
     { name: 'Sam Fleming', url: 'https://www.ft.com/stream/84aee01c-a582-3999-87d1-e3c4bbbb7d84' },
     { name: 'Lauren Leatherby', url: 'https://www.ft.com/lauren-leatherby' },
